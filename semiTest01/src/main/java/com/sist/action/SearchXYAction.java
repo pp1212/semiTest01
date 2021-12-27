8d7f095a22f8b758d7e03191dbfa91b5d1c34baf
pp1212 <pagy1218@hanmail.net>
2021-12-27 17:48:03 +0900

    SearchXYAction으로 받아온 xy로 Change_weatherAction 기온,강수량 출력 완성 - jy

--- semiTest01/src/main/java/com/sist/action/SearchXYAction.java
+++ semiTest01/src/main/java/com/sist/action/SearchXYAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sist.dao.LocationDAO;
 import com.sist.vo.LocationVO;
@@ -19,8 +20,10 @@ public class SearchXYAction implements SistAction {
 	public String proRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = ((HttpServletRequest)request).getSession();
 		
 		request.setCharacterEncoding("utf-8");
+		
 		String district = request.getParameter("district");
 		String dong = request.getParameter("dong");
 		
@@ -28,8 +31,23 @@ public class SearchXYAction implements SistAction {
 		int x_coord = l.getX_coord();
 		int y_coord = l.getY_coord();
 		
-		request.setAttribute("x_coord", x_coord);
-		request.setAttribute("y_coord", y_coord);
+		
+		if(session.getAttribute("x_coord") != null && session.getAttribute("y_coord") != null) {
+			session.removeAttribute("x_coord");
+			session.removeAttribute("y_coord");
+			
+			
+//			request.setAttribute("x_coord", x_coord);
+//			request.setAttribute("y_coord", y_coord);
+			
+			session.setAttribute("x_coord", x_coord);
+			session.setAttribute("y_coord", y_coord);
+		}else {
+			session.setAttribute("x_coord", x_coord);
+			session.setAttribute("y_coord", y_coord);
+		}
+		
+		
 		
 		return "change_weather.do";
 	}
